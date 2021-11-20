d82d2abe638edb2c80adb30d81877b2cb66efcb3
Dave Kaboyo <davekaboyok@gmail.com>
2021-11-20 09:52:19 +0300

    html and java script files

---
+++ index.js
@@ -0,0 +1,19 @@
+alert('Hey there, welcome');
+let year = 2021;
+let descripton = 9;
+console.log(year % descripton);
+
+let firstname=prompt('State your first name');
+let secondname=prompt('State your second name');
+alert(` That's great to hear ${firstname} ${secondname}`);
+
+let question=prompt("Why are you visiting this page?" );
+alert('Alright, that is great to know');
+
+let age=prompt('How old are you?'); 
+if (age>17){
+    alert("Oh, so you're an adult")
+}
+else {
+     alert("Oh, so you're a child")
+}
